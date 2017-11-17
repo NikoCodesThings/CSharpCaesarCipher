88e1acc27e3fcbb692bfe588bbeef95cf235e7e5
Nick <nespinosa976@gmail.com>
2017-11-16 19:52:45 -0500

    Rebuilt up to position

--- CaesarCipher/Program.cs
+++ CaesarCipher/Program.cs
@@ -14,8 +14,20 @@ namespace CaesarCipher
             //Console.WriteLine(StringExtensions.Let2Nat('z'));
             //Console.WriteLine(StringExtensions.Nat2Let(25));
             //Console.WriteLine(StringExtensions.Shift(3, 'z'));
-            Console.WriteLine(StringExtensions.Encode(3, "haskell is fun"));
-            Console.WriteLine(StringExtensions.Decode(3, "kdvnhoo lv ixq"));
+            //Console.WriteLine(StringExtensions.Encode(3, "haskell is fun"));
+            //Console.WriteLine(StringExtensions.Decode(3, "kdvnhoo lv ixq"));
+            //Console.WriteLine(StringExtensions.Lowers("haskell is fun"));
+            //Console.WriteLine(StringExtensions.Count('s', "haskell is fun"));
+            //int count = StringExtensions.Count('s', "haskell is fun");
+            //int lowers = StringExtensions.Lowers("haskell is fun");
+            //Console.WriteLine(StringExtensions.Percent(count, lowers));
+            String word = "haskell is fun";
+            float[] freqs = StringExtensions.Freqs(StringExtensions.Rotate(3, word));
+            /*for (int i = 0; i < 26; i++)
+                Console.WriteLine(freqs[i]);*/
+            //Console.WriteLine(StringExtensions.Rotate(3, "haskell is fun"));
+            Console.WriteLine(StringExtensions.Chisqr(freqs));
+
         }
     }
     public static class StringExtensions
@@ -67,5 +79,83 @@ namespace CaesarCipher
             return sb.ToString();
         }
 
+        // Calculates the total number of lower case letters in the word
+        public static int Lowers(String word)
+        {
+            int i = 0;
+
+            foreach(char c in word)
+            {
+                if (Char.IsLower(c))
+                    i++;
+            }
+
+            return i;
+        }
+        
+        // Calculates the number of times a given letter appears 
+        public static int Count(char letter, String word)
+        {
+            int i = 0;
+
+            foreach (char c in word)
+            {
+                if (c.Equals(letter))
+                    i++;
+            }
+
+            return i;
+        }
+
+        // Calculates the percentage of which a letter appears
+        public static float Percent(int count, int lowers)
+        {
+            return 100.0f * ((float)count / (float)lowers);
+        }
+
+        // Obtains the letter frequencies of an array
+        public static float[] Freqs(String word)
+        {
+            int count, lowers = Lowers(word);
+            float[] freqs = new float[26];
+
+            for (int i = 0; i < 26; i++)
+            {
+                count = Count(Nat2Let(i), word);
+
+                freqs[i] = Percent(count, lowers);
+            }
+
+            return freqs;
+        }
+        // Recursively rotates a string by a given amount
+        public static String Rotate(int amt, String word)
+        {
+            if (amt == 0)
+                return word;
+
+            return Rotate(amt - 1, word.Substring(1) + word.Substring(0, 1));
+        }
+
+        // Calculates the chisqr value from a given float array
+        public static float Chisqr(float[] input)
+        {
+            float chisqr = 0.0f;
+
+            for (int i = 0; i < 26; i++)
+                chisqr += (float)Math.Pow((double)input[i] - (double)table[i], 2.0) / table[i];
+
+            return chisqr;
+        }
+        // Determines the position of a value, if nothing is found, then -1 is returned
+        public static int Position(float value, float[] array)
+        {
+            for (int i = 0; i < array.Length; i++)
+            {
+                if (value.Equals(array[i]))
+                    return i;
+            }
+            return -1;
+        }
     }
 }
